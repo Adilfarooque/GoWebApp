db4bc0e0e4e9480b33afabe71782c125207aa9c9
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-10-20 21:30:46 +0530

    updated tempindex

--- tempindex/index.go
+++ tempindex/index.go
@@ -2,22 +2,36 @@ package tempindex
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
+var tpl *template.Template
 
-var tpl,_ = template.New("myTemplate").Funcs(template.FuncMap{
-	"lastItem": func(s []string) string {
-		lastIndex := len(s) - 1
-		return s[lastIndex]
-	},
-}).ParseFiles("index.html")
+func init() {
+	var err error
+
+	// Create a template function
+	tpl, err = template.New("myTemplate").Funcs(template.FuncMap{
+		"lastItem": func(s []string) string {
+			lastIndex := len(s) - 1
+			return s[lastIndex]
+		},
+	}).ParseFiles("tempindex/templates/index.html")
+
+	// log error
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 var g []string
 
 func IndexValue() {
 	g = []string{"Milk", "Coffe Poweder", "Mint", "Pottato", "Cheese", "Flour", "Sugar", "Salt", "Brocooli"}
-	tpl, _ = tpl.ParseGlob("templates/*.html")
-	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+
+	tpl, _ = tpl.ParseGlob("tempindex/templates/*.html")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tpl.ExecuteTemplate(w, "index.html", g)
 	})
 	http.ListenAndServe(":8080", nil)

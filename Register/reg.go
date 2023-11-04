0982f7454e7ba2a2f2a40bb753e118dbc2cf8dc1
Adifarooque <Adilfarq11@gmail.com>
2023-11-04 10:46:08 +0530

    Registering User Details With Some Criteria

---
+++ Register/reg.go
@@ -0,0 +1,143 @@
+package register
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var tpl *template.Template
+var db *sql.DB
+
+func reg() {
+	var err error
+	tpl, err = tpl.ParseGlob("./Register/template/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err = sql.Open("mysql", "root:password@tcp(localhost:3306)/testdb")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	//registerHandler serves from for registring new users
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("*****registerHandler running*****")
+		tpl.ExecuteTemplate(w, "register.html", nil)
+	})
+	//registerAuthHandler creates new user in database
+	http.HandleFunc("/registerauth", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("*****registerAuthHandlerRunning*****")
+		r.ParseForm()
+		username := r.FormValue("username")
+		//check username for only alphaNumeric
+		var nameAlphaNumeric = true
+		for _, char := range username {
+			//func IsLetter(r rune)bool,func IsNumber(r rune)bool
+			//if !unicode.IsLetter(char)&& unicode.IsNumber
+			if unicode.IsLetter(char) == false && unicode.IsNumber(char) == false {
+				nameAlphaNumeric = false
+			}
+		}
+
+		//check username pswdLength
+		var nameLength bool
+		if 5 <= len(username) && len(username) >= 50 {
+			nameLength = true
+		}
+
+		//check password criteria
+		password := r.FormValue("password")
+		fmt.Println("password:", password, "\npswdLength:", len(password))
+		//variable that must pass for password creation criteria
+		var pswdLowercase, pswdUppercase, pswdNumber, pswdSpecial, pswdLength, pswdSpaces bool
+		pswdSpaces = true
+
+		for _, char := range password {
+			switch {
+			// func IsLower(r rune)bool
+			case unicode.IsLower(char):
+				pswdLowercase = true
+			// func IsUpper(r rune)bool
+			case unicode.IsUpper(char):
+				pswdUppercase = true
+				// func IsNumber(r rune)bool
+			case unicode.IsNumber(char):
+				pswdNumber = true
+				// func IsPunct(r rune)bool , // func IsSymbol(r rune)bool
+			case unicode.IsPunct(char) || unicode.IsSymbol(char):
+				pswdSpecial = true
+				//func IsSpace (r rune)bool,type rune = int32
+			case unicode.IsSpace(int32(char)):
+				pswdSpaces = false
+
+			}
+		}
+
+		if 11 < len(password) && len(password) < 60 {
+			pswdLength = true
+		}
+
+		if !pswdLowercase || !pswdUppercase || !pswdNumber || !pswdSpecial || !pswdSpaces || !pswdLength {
+			tpl.ExecuteTemplate(w, "register.html", "Please check username and password criteria")
+			return
+		}
+
+		//check if username already exists for availability
+		stmt := "SELECT UserID FROM bcrypt WHERE username = ?"
+		row := db.QueryRow(stmt, username)
+
+		var uID string
+		err := row.Scan(&uID)
+
+		if err != sql.ErrNoRows {
+			fmt.Println("username already exists , err:", err)
+			tpl.ExecuteTemplate(w, "register.html", "username already taken")
+			return
+		}
+
+		//create hash from password
+		var hash []byte
+
+		//
+		hash, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			//fmt.Println("bcrypt err:",err)
+			tpl.ExecuteTemplate(w, "register.html", "There was a problem registering account")
+			return
+		}
+		fmt.Println("hash:", hash)
+		fmt.Println("String(hash):", string(hash))
+
+		//func (db *DB)prepare(query sting)(*stmt, error)
+		var insertStmt *sql.Stmt
+		insertStmt, err = db.Prepare("INSERT INTO bcrypt(username , Hash) VALUES (?,?);")
+		if err != nil {
+			fmt.Println("Error Prepareing Statement :", err)
+			tpl.ExecuteTemplate(w, "register.html", "Ther was a problem registering account")
+			return
+		}
+		defer insertStmt.Close()
+
+		var result sql.Result
+		//func (s *stmt)Exces(arg ...interface{})(result,error)
+		result, err = insertStmt.Exec(username, hash)
+		rowsaff, _ := result.RowsAffected()
+		lastIns, _ := result.LastInsertId()
+		fmt.Println("rowsAff:", rowsaff)
+		fmt.Println("lastIns:", lastIns)
+		fmt.Println("err", err)
+		if err != nil {
+			fmt.Println("error inserting new user")
+			tpl.ExecuteTemplate(w, "reginster.html", "There was a problem registering accoutn")
+			return
+		}
+		fmt.Fprint(w, "congrats , your account has been successfully created")
+
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GoWebApp
 
 go 1.20
+
+require golang.org/x/crypto v0.14.0 // indirect

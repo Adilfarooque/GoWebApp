566c2a4d9fc69775c8f917556b37c8bd90a802d8
Adifarooque <Adilfarq11@gmail.com>
2023-11-12 17:48:35 +0530

    Just Updated

---
+++ muxServer/Server.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	file := http.FileServer(http.Dir("templates"))
+	mux.Handle("/", file)
+	http.ListenAndServe(":8080", mux)
+}
